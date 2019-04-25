95d6000127f64b983b02783858853d0e4a567213
Abhiram <49971631+AbhiramKumar@users.noreply.github.com>
2019-04-25 21:18:47 +0530

    Initial Commit
    In this initial commit i created process structure.
    Which having properties arrival time, burst time, completion time, turnaround time, waiting time.
    
    In this commit i ask count of processes and processes arrival, burst, queue, and then using get_max_queue function i can calculate no of queues having in my OS.

---
+++ Program.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <stdio.h>
+#include <Windows.h>
+
+struct processes
+{
+    short int arrival, burst, burst_backup, completion = 0, turn_around = 0, waiting = 0, response = 0, queue;
+} * process;
+
+short int total_time = 0, time_quantum = 2, processes_count, queue_count;
+
+int get_max_queue()
+{
+    int max = 0;
+    for (short int index = 0; index < processes_count; index++)
+    {
+        if (process[index].queue > max)
+        {
+            max = process[index].queue;
+        }
+    }
+
+    return max;
+}
+
+int main()
+{
+    printf("Enter Processes Count : ");
+    scanf("%d", &processes_count);
+    process = new processes[processes_count];
+    printf("\nEnter Foramt AT BT QN (Starts from 0)\n");
+
+    for (short int index = 0; index < processes_count; index++)
+    {
+        printf("\nP%d details : ", index);
+        scanf("%d %d %d", &process[index].arrival, &process[index].burst, &process[index].queue);
+        process[index].burst_backup = process[index].burst;
+    }
+
+    system("cls");
+    queue_count = get_max_queue();
+    system("pause");
+}
